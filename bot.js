a53f94921ea18b2ee59c447d382276cfda19e4d4
Murat <kaid.murat3@gmail.com>
2018-01-22 18:25:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    var letze = message.content;
     if (message.content === 'ping') {
     	message.channel.send('pong');
   	}
@@ -21,12 +22,17 @@ client.on('message', message => {
     if (message.content === 'cp' || message.content === 'Cp') {
     	message.channel.send('https://www.youtube.com/channel/UChUSMj5WAeooPfhRqvEjAkw');
   	}
-    if (message.content === 'erens lol account' || message.content === 'Erens lol account') {
-        var text1 = "Erens Lol Account ist ";
+    if (message.content === 'erens lol account' || message.content === 'Erens lol account'|| message.content === 'Erens lol Account') {
+        var text1 = 'Erens Lol Account ist noch ';
         var text2 = 9000-yyyy;
-        var text3 = " Jahre gebannt!";
+        var text3 = ' Jahre gebannt!';
         var ausgabe = text1 + text2 + text3;
     	message.channel.send(ausgabe);
+  	}
+    if (message.content === 'Warum?' || message.content === 'warum?') {
+    	if (letze == 'erens lol account' || message.content === 'Erens lol account'|| message.content === 'Erens lol Account') {
+            message.channel.send('weil sein Freund gecheatet hat /:laugh:');
+        }
   	}
 });
 
