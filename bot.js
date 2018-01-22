d5730a290b6fa9c0f52bd00691a0cedcc661f5e3
Murat <kaid.murat3@gmail.com>
2018-01-22 18:01:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var today = new Date();
+var yyyy = today.getFullYear();
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -9,8 +12,17 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
-    if (message.content === 'emre') {
-    	message.reply('behindert');
+    if (message.content === '?') {
+    	message.reply('Befehle:\n? - was ich kann\nemre\ncp\nerens lol account');
+  	}
+    if (message.content === 'emre' || message.content === 'Emre') {
+    	message.reply('Emre ist behindert, ja!');
+  	}
+    if (message.content === 'cp' || message.content === 'Cp') {
+    	message.reply('https://www.youtube.com/channel/UChUSMj5WAeooPfhRqvEjAkw');
+  	}
+    if (message.content === 'erens lol account' || message.content === 'Erens lol account') {
+    	message.reply('Erens lol Account ist $9000-yyyy Jahre gebannt');
   	}
 });
 
